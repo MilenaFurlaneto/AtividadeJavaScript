0a9e6f5ecce6d2d17ef6f259e105b6cb3ca1b6ab
Milena da Silva Fulaneto <132371316+MilenaFurlaneto@users.noreply.github.com>
2023-05-25 11:19:44 -0300

    Add files via upload

---
+++ exercicio5.js
@@ -0,0 +1,8 @@
+matriz = [];
+matriz [0] = ["Shrek", "Animação", 2001 ]
+matriz [1] = ["Vingadores", "Aventura", 2012 ]
+matriz [2] = ["Toy Story", "Fantasia", 1995]
+matriz [3] = ["Clueless", "Comédia Romantica", 1995]
+matriz [4] = ["O Diabo veste Prada", "Comédia Romantica", 2006 ] 
+console.log (matriz)
+
